148d6c40f7ed912596540d96eaa7975d67067608
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-05-16 09:26:05 +0200

    Create InvoiceEntity and StatusEntity

---
+++ WebApi/Data/Entities/InvoiceEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Data.Entities;
+
+public class InvoiceEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string BookingId { get; set; } = null!;
+    public string InvoiceTitel { get; set; } = null!;
+
+    //public bool IsPaid { get; set; }
+
+    //public DateTime BookingDate { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    //Invoice Details should be the same as the Booking Details (exists in another API)
+
+    [ForeignKey(nameof(Status))]
+    public int StatusId { get; set; }
+    public StatusEntity Status { get; set; } = null!;
+}
