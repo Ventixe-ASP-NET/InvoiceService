d900642ad7a5efd0ec406c69aee2159ae58d821b
LuizOliveir4 <luiz_oliveira.k@hotmail.com>
2025-05-20 21:37:12 +0200

    Insert FunctionsWorkerMiddleware

--- InvoiceService/Program.cs
+++ InvoiceService/Program.cs
@@ -1,13 +1,18 @@
 using InvoiceService.Data.Contexts;
 using InvoiceService.Services;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWebApplication(builder =>
+    {
+    builder.UseMiddleware<CorsMiddleware>();
+    })
     .ConfigureServices((hostContext, services) =>
     {
         services.AddDbContext<DataContext>(options =>
@@ -23,3 +28,25 @@ var host = new HostBuilder()
     .Build();
 
 host.Run();
+
+// Middleware de CORS para Azure Functions dotnet-isolated
+public class CorsMiddleware : IFunctionsWorkerMiddleware
+{
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        var httpRequest = await context.GetHttpRequestDataAsync();
+        if (httpRequest != null)
+        {
+            context.Items["__AddCorsHeaders"] = true;
+        }
+
+        await next(context);
+
+        if (httpRequest != null && context.GetHttpResponseData() is { } response)
+        {
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        }
+    }
+}
