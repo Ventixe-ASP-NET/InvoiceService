48119c54fce2e3e6152784041d1a2fe29a4a2de8
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-05-27 12:15:51 +0200

    Fix: Create _context that includes Status

--- InvoiceService/Data/Repositories/InvoiceRepository.cs
+++ InvoiceService/Data/Repositories/InvoiceRepository.cs
@@ -1,8 +1,22 @@
 ﻿using InvoiceService.Data.Contexts;
 using InvoiceService.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceService.Data.Repositories;
 
 public class InvoiceRepository(DataContext context) : BaseRepository<InvoiceEntity>(context)
 {
+    public override async Task<IEnumerable<InvoiceEntity>> GetAllAsync()
+    {
+        return await _context.Invoices
+            .Include(i => i.Status)
+            .ToListAsync();
+    }
+
+    public override async Task<InvoiceEntity?> GetAsync(System.Linq.Expressions.Expression<Func<InvoiceEntity, bool>> expression)
+    {
+        return await _context.Invoices
+            .Include(i => i.Status)
+            .FirstOrDefaultAsync(expression);
+    }
 }
